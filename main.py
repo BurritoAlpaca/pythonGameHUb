c43310a0534f59ccdef504c19e0e9ae355f56714
BurritoAlpaca <burritoalpaca25@gmail.com>
2022-02-02 21:44:09 +0000

    b

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ player1 = Player()
 
 gameList = ['Dice']
 
-#file1 = open('GameRules.txt', 'r')
+file1 = open('GameRules.txt', 'r')
 
-#print(file1[0])
+print(file1[0])
 
 def startGame():
     print('Welcome {0}\nWhat game would you like to play?'.format(player1.name))
@@ -20,7 +20,7 @@ def startGame():
         print('\n{0}: {1}'.format(i + 1, gameList[i]))
 
 def gameSelect(gameNum):
-    if gameNum = 0:
+    if gameNum == 0:
         dice()
 
 def dice():
